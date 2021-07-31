50001ee0245dd757b840e95a90fd5f6d0ad2d71a
18B080013J <jaypatil262@gmail.com>
2021-07-31 16:55:59 +0530

    commit

--- shany/settings.py
+++ shany/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#^yr05(cr0*oz$znkfzdiqhicx(s9f(+-$1&r69=_fu_m*ryag
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1:8000','bobmf.herokuapp.com']
 
 
 # Application definition
